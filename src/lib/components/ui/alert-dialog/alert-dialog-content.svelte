c3285b82c30e8c2115a568e04d4b0e1f75a235ee
Dmitry Zhomir <giantdem1@gmail.com>
2025-07-07 00:06:55 +0400

    dialog blur & config'able position

--- src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
@@ -7,10 +7,17 @@
 		ref = $bindable(null),
 		class: className,
 		portalProps,
+        position = "center",
 		...restProps
 	}: WithoutChild<AlertDialogPrimitive.ContentProps> & {
 		portalProps?: WithoutChildrenOrChild<AlertDialogPrimitive.PortalProps>;
+        position?: "center" | "bottom-right";
 	} = $props();
+
+    const positionClasses = {
+        center: "fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
+        "bottom-right": "fixed bottom-16 right-3"
+    };
 </script>
 
 <AlertDialogPrimitive.Portal {...portalProps}>
@@ -19,7 +26,8 @@
 		bind:ref
 		data-slot="alert-dialog-content"
 		class={cn(
-			"bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed left-[50%] top-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+			"bg-background/90 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 z-50 grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+            positionClasses[position] ?? positionClasses.center,
 			className
 		)}
 		{...restProps}
