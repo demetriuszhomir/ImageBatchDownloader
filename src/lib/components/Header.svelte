d6c0c332e3230db811dfc43793b60391bb568558
Dmitry Zhomir <giantdem1@gmail.com>
2025-07-18 18:18:02 +0400

    header bg fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,7 +7,7 @@
     export let toggleTheme: () => void;
 </script>
 
-<header class="fixed top-0 left-0 right-0 z-20 backdrop-blur bg-background/80 border-b p-3 flex items-center justify-between min-h-0 h-12">
+<header class="fixed top-0 left-0 right-0 z-20 backdrop-blur bg-background/90 border-b p-3 flex items-center justify-between min-h-0 h-12">
     <div class="flex items-center gap-2">
         <img src={logoUrl} alt="Logo" class="w-7 h-7 shrink-0" />
         <span class="font-semibold select-none max-[20rem]:hidden">Image&nbsp;Batch&nbsp;Downloader</span>
