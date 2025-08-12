a684b53880238e2ddd02e2fd1cb945163a99e53d
Dmitry Zhomir <giantdem1@gmail.com>
2025-08-12 23:27:15 +0400

    lp components splitting

---
+++ web/src/lib/components/landing/FeatureCategory.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { Component } from "svelte";
+    import FeatureTile from "./FeatureTile.svelte";
+
+    type Item = { icon: Component<any>; text: string };
+    type Props = { title: string; items: Item[] };
+
+    let { title, items }: Props = $props();
+</script>
+
+<div>
+    <div class="text-base font-semibold mb-2">{title}</div>
+    <div class="grid grid-cols-1 min-[20rem]:grid-cols-2">
+        {#each items as item}
+            <FeatureTile Icon={item.icon} text={item.text} />
+        {/each}
+    </div>
+</div>
