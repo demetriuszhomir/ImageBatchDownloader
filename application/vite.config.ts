3e7cb1b20970bfee2fd9e711bb96d6c03a3a68fb
Dmitry Zhomir <giantdem1@gmail.com>
2025-08-17 00:18:58 +0400

    app imgs optimized

--- application/vite.config.ts
+++ application/vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,5 +8,5 @@ export default defineConfig({
         assetsInlineLimit: Infinity
     },
 
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()]
 });
